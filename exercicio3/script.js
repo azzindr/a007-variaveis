9ce401f22ac758ad59ebff5ec5aeb542d5bc89d2
André F. Azzi <andreazziarq@gmail.com>
2022-07-20 16:23:36 -0300

    exercicios prontos

---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 25;
+let c;
+
+c=a //usamos uma variável "vazia" para conseguir alterar o valor das demais variáveis/
+a=b
+b=c
+
+console.log ("O novo valor de a é", a)
+console.log ("O novo valor de b é", b)
